83622e52886a333a3a6977bb4f4fcf47ee49e4cd
princijain11 <67953935+princijain11@users.noreply.github.com>
2020-07-25 17:15:44 +0530

    Create hellowrold.py

---
+++ hellowrold.py
@@ -0,0 +1,8 @@
+# This program prints Hello, world!
+
+print('Hello, world! 1')
+print('Hello, world! 2')
+print('Hello, world! 3')
+print('Hello, world! 4')
+print('Hello, world! 5')
+print('Hello, world! 6')
