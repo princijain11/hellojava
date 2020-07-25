2d415fe076e5ccd8ac2619f29027b8714c4dd421
princijain11 <67953935+princijain11@users.noreply.github.com>
2020-07-25 16:08:57 +0530

    Create Simple.java

---
+++ Simple.java
@@ -0,0 +1,16 @@
+class Simple{
+public static void main(String args[]){
+System. out. println("Hello Java1");
+System. out. println("Hello Java2");
+System. out. println("Hello Java3");
+System. out. println("Hello Java4");
+System. out. println("Hello Java5");
+System. out. println("Hello Java6");
+System. out. println("Hello Java7");
+System. out. println("Hello Java8");
+System. out. println("Hello Java9");
+System. out. println("Hello Java10");
+System. out. println("Hello Java11");
+System. out. println("Hello Java12");
+}
+}
